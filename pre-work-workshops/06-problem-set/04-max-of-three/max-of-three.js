f2cd543e4df10ec850d782954671b4302824a03e
anish-X <anish.stha527@gmail.com>
2025-07-08 11:54:37 +0545

    add max-of-three

--- pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+
+const maxOfThree = (num1, num2, num3) => {
+    let max = 0;
+    if(num1 > num2){
+        max = num1
+    }else{
+        max = num2
+    }
+    max = ( max > num3) ? max: num3;
+    return max;
+}
