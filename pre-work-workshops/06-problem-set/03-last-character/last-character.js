5e8b902caa1df501cb153ffe770cceda1ecadf72
anish-X <anish.stha527@gmail.com>
2025-07-08 11:54:26 +0545

    add last character

--- pre-work-workshops/06-problem-set/03-last-character/last-character.js
+++ pre-work-workshops/06-problem-set/03-last-character/last-character.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+const lastCharacter = (arg1, arg2) => {
+    if(arg1[arg1.length -1] ===  arg2[arg2.length - 1]){
+        return true;
+    }else{
+        return false;
+    }
+}
