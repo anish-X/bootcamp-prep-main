c7a598fcec756fcd231b5b4341f5b5b8ab120978
anish-X <anish.stha527@gmail.com>
2025-07-09 17:37:59 +0545

    add is truthy

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+const isTruthy = (value) => {
+    if(value){
+        return true;
+    }else if(value === null){
+        return `The ${value} value is falsey`;
+    } else if(value === false){
+        return "The boolean value false is falsey";
+    }else if (value === 0){
+        return "The number 0 is falsey (the only falsey number)";
+    }else if(value === ''){
+        return "The empty string is falsey (the only falsey string)";
+    }else{
+        return "undefined is falsey";
+    }
+}
