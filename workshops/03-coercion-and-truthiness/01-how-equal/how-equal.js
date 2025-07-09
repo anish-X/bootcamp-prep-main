1e4070977eb264a0328d6d9f5c7cf49c7dcb3652
anish-X <anish.stha527@gmail.com>
2025-07-09 17:37:49 +0545

    add how equal

--- workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+const howEqual = (args1, args2) => {
+    if(args1 === args2){
+        return 'strictly';
+    }else if (args1 == args2){
+        return 'loosely';
+    }else{
+        return "not equal";
+    }
+}
