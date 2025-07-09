8f331ca401789d84f30ccb9c7a9115268725148d
anish-X <anish.stha527@gmail.com>
2025-07-09 17:38:27 +0545

    add zero dark thirty

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+const zeroDarkThirty = (value) => {
+    let valueString = value.toString();
+    let noZeroValue = '';
+    if(value === 0){
+        return NaN;
+    }
+    for(let value of valueString){
+        if(value == 0){
+            continue;
+        }
+        noZeroValue += value;
+    }
+
+    return Number(noZeroValue);
+}
