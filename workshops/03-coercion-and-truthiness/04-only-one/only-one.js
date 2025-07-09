9819c814d9d080d116e91c3a10e1cc3c39b188ca
anish-X <anish.stha527@gmail.com>
2025-07-09 17:38:17 +0545

    add only one

--- workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,29 @@
 // YOUR CODE BELOW
+const onlyOne = (arg1, arg2, arg3) => {
+    // if(arg1 && !arg2 && !arg3){
+    //     return true;
+    // }else if( !arg1 && arg2 && !arg3){
+    //     return true;
+    // }else if(!arg1 && !arg2 && arg3){
+    //     return true;
+    // }else {
+    //     return false;
+    // }
+   
+
+    if(!arg1 && arg2 && arg3){
+        return false;
+    }else if( arg1 && !arg2 && arg3){
+        return false;
+    }else if(arg1 && arg2 && !arg3){
+        return false;
+    }else if(!arg1 && !arg2 && !arg3){
+        return false;
+    }else if(arg1 && arg2 && arg3){
+        return false;
+    }else{
+        return true;
+    }
+    
+}
+
